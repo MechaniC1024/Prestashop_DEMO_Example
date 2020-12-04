3a8eb787d3de62454a00ed7eda8ddd809549a862
MechaniC1024 <bog2641@yandex.ru>
2020-12-05 02:10:34 +0300

    Add project.

---
+++ src/test/framework/com/PrestaShop/AllureAttach/AllureUUID.java
@@ -0,0 +1,7 @@
+package com.PrestaShop.AllureAttach;
+
+import java.io.File;
+
+public interface AllureUUID {
+	public static final File file = new File("src/test/resources/AllureUUID/config.properties");
+}
---
+++ src/test/framework/com/PrestaShop/AllureAttach/ReadWriteProperty.java
@@ -0,0 +1,36 @@
+package com.PrestaShop.AllureAttach;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
+public class ReadWriteProperty {
+	
+	public static void write(String key, String value) {
+
+		Properties prop = new Properties();
+
+		try (OutputStream output = new FileOutputStream(AllureUUID.file, true);){
+	        prop.setProperty(key, value);
+	        prop.store(output, null);
+	    } catch (IOException io) {
+	        io.printStackTrace();
+	    } 
+	}
+	
+	public static String read(String key) {
+		
+		Properties prop = new Properties();
+		
+	    try (InputStream input = new FileInputStream(AllureUUID.file);){
+	      prop.load(input);
+	    } catch (IOException ex) {
+	        ex.printStackTrace();
+	    }
+	    
+	    return prop.getProperty(key);
+	}	
+}
